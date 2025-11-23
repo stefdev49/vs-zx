1cc5bbb234503980b8cdbcb66cc99e9dac0cd98d
stef <stefdev49@gmail.com>
2025-11-23 15:00:52 +0100

    cleanups

--- converter/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function convertToBinary(basicText: string): Buffer;
--- rs232-transfer/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function transfer(binary: Buffer, portPath: string, baudRate?: number): Promise<void>;
